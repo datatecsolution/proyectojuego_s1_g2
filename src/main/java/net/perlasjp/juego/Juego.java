a625da8a4e31112bf71cfcb1d466c7f94803b0d5
Daniela <matheudaniela9@gmail.com>
2025-08-20 08:39:00 -0600

    Juego

---
+++ src/main/java/net/perlasjp/juego/Juego.java
@@ -0,0 +1,18 @@
+package net.perlasjp.juego;
+
+public class juego{
+    private jugador jugador;
+    private Enemigo enemigo;
+    private Combate combate;
+
+    public void iniciar (){
+        
+    }
+}
+     
+     
+
+
+    
+
+
