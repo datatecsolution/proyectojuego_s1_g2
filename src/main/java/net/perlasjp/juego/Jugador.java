fce5449341e5442ff730e65fa4d8438d6ea925e7
EduardoFlores0724 <gemereduar456@gmail.com>
2025-08-20 08:20:02 -0600

    agregue parametros

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,5 +1,39 @@
 package net.perlasjp.juego;
 
 public class Jugador {
+    private String nombre;
+    private int salud;
+
+    public int atacar() {
+        return 0;
+
+    }
+
+    public void recibirDanio(int danio) {
+
+    }
+
+    public boolean estavivo() {
+        return false;
+
+    }
+
+    public void Jugador(String n) {
+
+    }
+
+    public String getNombre() {
+        return false;
+
+    }
+
+    public int getSalud() {
+        return false;
+
+    }
+
+    public void mostrarEstado() {
+
+    }
 
 }
