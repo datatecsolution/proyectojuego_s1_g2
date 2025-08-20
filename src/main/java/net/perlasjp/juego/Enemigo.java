49b657fe2239d26d442c2d4943aaf70834aa215b
Andrea Diaz <lovonepta@gmail.com>
2025-08-20 08:32:55 -0600

    agregue enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -2,4 +2,22 @@ package net.perlasjp.juego;
 
 public class Enemigo {
     
+    private String nombre;
+    private int salud; 
+    private int fuerza;
+
+    public int atacar(){
+
+    }
+
+    public void recibirDanio(int danio){
+
+    }
+
+    public boolean estaVivo(){
+
+    }
+    
+
+
 }
